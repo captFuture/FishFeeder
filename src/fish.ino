f6d4508fe476717d5a32898aef29ed5c582e0070
Chris Tarantl <profsimon@gmail.com>
2020-02-04 11:36:39 +0100

    telemetrie

--- src/fish.ino
+++ src/fish.ino
@@ -16,7 +16,7 @@
 #define RPM 120
 #define MICROSTEPS 1
 
-StaticJsonBuffer<200> jsonBuffer;
+StaticJsonBuffer<500> jsonBuffer;
 A4988 stepper(stepsPerRevolution, dirPin, stepPin, enablePin);
 
 WiFiManager wifiManager;
@@ -43,8 +43,8 @@ void callback(char* topic, byte* payload, unsigned int length) {
     Serial.println();
     buff_p[length] = '\0';
     String msg_p = String(buff_p);
-    degrees = msg_p.toInt(); // to Int
-    turned = 0;
+    int manualdegrees = msg_p.toInt(); // to Int
+    turnMotor(manualdegrees, 1);
 
   }else if(mqttopic == configTopic){
     char buff_p[length];
@@ -56,6 +56,8 @@ void callback(char* topic, byte* payload, unsigned int length) {
     buff_p[length] = '\0';
     String msg_p = String(buff_p);
     Serial.println(msg_p); 
+
+    SPIFFS.begin();
     File file = SPIFFS.open("/config.json", "w");
  
     if (!file) {
@@ -75,22 +77,47 @@ void callback(char* topic, byte* payload, unsigned int length) {
   
     file.close();
   }else if(mqttopic == rebootTopic){
-    ESP.restart();
+    char buff_p[length];
+    for (int i = 0; i < length; i++) {
+      Serial.print((char)payload[i]);
+      buff_p[i] = (char)payload[i];
+    }
+    Serial.println();
+    buff_p[length] = '\0';
+    String msg_p = String(buff_p);
+    int command = msg_p.toInt(); // to Int
+    if(command == 0){
+      Serial.println("TODO: http update firmware");
+    }else{
+      ESP.restart();
+    }
   }
 }
 
 void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
+    Serial.print("Attempting MQTT connection to - ");
+    Serial.print(mqtt_server);
+    Serial.println(" - ");
+    Serial.print(mqtt_user);
+    Serial.println(" - ");
+    Serial.print(mqtt_pwd);
+    Serial.println(" - ");
+    Serial.print("in_topic: "); Serial.println(in_topic);
+    Serial.print("out_topic: "); Serial.println(out_topic);
+    Serial.print("config_topic: "); Serial.println(config_topic);
     
     
     if (client.connect(clientID,mqtt_user, mqtt_pwd)) {
+    //if (client.connect(clientID,"megara", "Che11as!1")) {
       Serial.println("connected");
       client.subscribe(in_topic);
       client.subscribe(config_topic);
       client.subscribe(reboot_topic);
-      client.publish(out_topic, "Hello I am here");
+      snprintf (msg, 50, "Connected");
+      
+      postTelemetry(msg);
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -103,9 +130,13 @@ void reconnect() {
 
 void readSpiffs(){
   //read configuration from FS json
-    Serial.println("mounting FS...");
+    Serial.println("\nmounting FS...");
     if (SPIFFS.begin()) {
       Serial.println("mounted file system");
+
+      //Serial.println("formatting file system");
+      //SPIFFS.format();
+
       if (SPIFFS.exists("/config.json")) {
         //file exists, reading and loading
         Serial.println("reading config file");
@@ -127,15 +158,37 @@ void readSpiffs(){
             startHour = json["start"];
             endHour = json["end"];
 
+            strcpy(clientID, json["clientID"]);
+            strcpy(mqtt_server, json["mqtt_server"]);
+            strcpy(mqtt_user, json["mqtt_user"]);
+            strcpy(mqtt_pwd, json["mqtt_pwd"]);
+
+            strcpy(out_topic, json["out_topic"]);
+            strcpy(in_topic, json["in_topic"]);
+            strcpy(config_topic, json["config_topic"]);
+            strcpy(reboot_topic, json["reboot_topic"]);
+
             Serial.println(degrees);
             Serial.println(interval);
             Serial.println(startHour);
             Serial.println(endHour);
-            //strcpy(output, json["output"]);
+
+            Serial.println(clientID);
+            Serial.println(mqtt_server);
+            Serial.println(mqtt_user);
+            Serial.println(mqtt_pwd);
+
+            Serial.println(out_topic);
+            Serial.println(in_topic);
+            Serial.println(config_topic);
+            Serial.println(reboot_topic);
+            
           } else {
             Serial.println("failed to load json config");
           }
         }
+      }else{
+        Serial.println("confignot present");
       }
     } else {
       Serial.println("failed to mount FS");
@@ -145,10 +198,7 @@ void readSpiffs(){
 
 void setup() {
     Serial.begin(115200);
-    while (!Serial) 
-    { 
-      ; // Wait for serial to connect 
-    }
+    while (!Serial){};
     readSpiffs();
     stepper.begin(RPM, MICROSTEPS);
     stepper.setEnableActiveState(LOW);
@@ -175,56 +225,58 @@ void loop() {
     loopHour = timeClient.getHours();
     loopMinute = timeClient.getMinutes();
     loopEpochTime = timeClient.getEpochTime();
-    //Serial.print("Loop epoch:"); Serial.println(loopEpochTime); 
-
-    //Serial.print("Loop hours:"); 
-    //Serial.println(loopHour);
 
       // Rotate Motors if interval in minutes is over
       if(currentEpochTime != -1){
-        //Serial.print("Current epoch:"); Serial.println(currentEpochTime); 
-        //Serial.print("calculus var:"); Serial.println(currentEpochTime+(interval*60)); 
         if(loopEpochTime > (currentEpochTime+interval*60)){
           if(loopHour >= startHour && loopHour <= endHour){
-            Serial.println("FeedTheFish epochTime");
+            Serial.println("FeedTheFish");
             feedTheFish();
             currentEpochTime = timeClient.getEpochTime();
           }
         }
       }
 
-  if(turned == 0){
-    Serial.println("Turning in loop");
-    turnMotor();
-  }
-
   delay(1000);
 }
 
 void feedTheFish(){
-  //do something to turn the motor
-  //degrees = ;
+  //Feed the fish automatically
   turned = 0;
-  turnMotor();
+  turnMotor(degrees, 0);
 }
 
-void turnMotor(){
+void turnMotor(int variable, int turnmode){
+  Serial.print("Start: "); Serial.print(variable);
   stepper.enable();
   delay(1000);
-  Serial.print("-45:");
   stepper.rotate(-45);
-  Serial.println("45: ");
   delay(1000);
   stepper.rotate(45);
-  //delay(2000);
-  Serial.print("Start : "); Serial.println(degrees);
-  stepper.rotate(degrees);
-  if(enableWifi){
-    snprintf (msg, 50, "turned: %d", degrees);
-    Serial.print("Publish message: ");
-    Serial.println(msg);
-    client.publish(out_topic, msg);
-  }
+  stepper.rotate(variable);
+  snprintf (msg, 50, "Motor turned:%d:%d", variable, turnmode);
+  postTelemetry(msg);
   turned = 1;
   stepper.disable();
+  Serial.println(" : End");
+}
+
+void postTelemetry(char msg[]){
+  String payload = "{";
+  payload += "\"clientID\":"; payload += "\""; payload += clientID; payload += "\"";
+  payload += ",";
+  payload += "\"start\":"; payload += startHour;
+  payload += ",";
+  payload += "\"end\":"; payload += endHour;
+  payload += ",";
+  payload += "\"interval\":"; payload += interval;
+  payload += ",";
+  payload += "\"weight\":"; payload += degrees;
+  payload += ",";
+  payload += "\"message\":"; payload; payload += "\""; payload += msg; payload += "\"";
+  payload += "}";
+
+  char attributes[200];
+  payload.toCharArray( attributes, 200 );
+  client.publish(out_topic, attributes);
 }
--- src/variables.h
+++ src/variables.h
@@ -1,23 +1,26 @@
 
-const char* clientID = "FishFeeder_001";
-const char* mqtt_server = "makeradmin.ddns.net";
-const char* mqtt_user = "linaro";
-const char* mqtt_pwd = "Che11as!1";
-const char* out_topic = "fish/001/out";
-const char* in_topic = "fish/001/in";
-const char* config_topic = "fish/001/config";
-const char* reboot_topic = "fish/001/reboot";
+char version[6] = "0.0.1";    // Firmware version
+
+char clientID[] = "FishFeeder_initial";
+char mqtt_server[] = "mrtarantl.ddns.net";
+char mqtt_user[] = "megara";
+char mqtt_pwd[] = "Che11as!1";
+
+char out_topic[] = "fish/initial/telemetry";
+char in_topic[] = "fish/initial/command";
+char config_topic[] = "fish/initial/config";
+char reboot_topic[] = "fish/initial/reboot";
 
 long lastMsg = 0;
-char msg[50];
+char msg[500];
 int degrees = 360;
 int interval = 10;
-int startHour = 0;
-int endHour = 0;
+int startHour = 6;
+int endHour = 18;
 int turned = 1;             // set to 0 and it rotates on startup
 bool enableWifi = true;     // standalone mode without wifi -> false ... not working at the moment
 
-bool debugMode = true;      // if debug is on, it rotates every minute instead every hour
+bool debugMode = true;      
 int currentHour = -1;
 int currentMinute = -1;
 int currentEpochTime = -1;
